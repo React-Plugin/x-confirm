4cb24973da719a444bef7c5ef56987d00343b75b
tianxiangbing <55342775@qq.com>
2017-05-26 18:40:41 +0800

    init

---
+++ src/components/Confirm.jsx
@@ -0,0 +1,22 @@
+/*
+ * Created with Visual Studio Code.
+ * github: https://github.com/React-Plugin/x-seed
+ * User: 田想兵
+ * Date: 2017-05-14
+ * Time: 20:00:00
+ * Contact: 55342775@qq.com
+ */
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+// import Style from './_Seed'; 
+
+export default class Seed extends Component {
+  render () {
+    console.log(123)
+    return (
+      <div className="Seed">
+        hello world...<a href="https://github.com/React-Plugin/x-seed">https://github.com/React-Plugin/x-seed</a>
+      </div>
+    );
+  }
+}
---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+/*
+ * Created with Visual Studio Code.
+ * github: https://github.com/React-Plugin/x-confirm
+ * User: 田想兵
+ * Date: 2017-05-14
+ * Time: 20:00:00
+ * Contact: 55342775@qq.com
+ */
+import Confirm from './components/Confirm';
+module.exports = Confirm;  //使用module.exports时，从es6到es5生成的dist不会出现export.default的问题.
